99abb7960ca865c1c98684ce8ee9bae229788f64
Josh Wise <josh.wise@proof.com>
2025-05-27 16:16:55 -0700

    Support Autocorrect in RequireFrozenStringLiteral (#15)

--- lib/rubocop/cop/performance/require_frozen_string_literal.rb
+++ lib/rubocop/cop/performance/require_frozen_string_literal.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
 
-require 'pry'
-
 module RuboCop
   module Cop
     module Performance
@@ -19,20 +17,48 @@ module RuboCop
       class RequireFrozenStringLiteral < Base
         include RangeHelp
         include FrozenStringLiteral
+        extend RuboCop::Cop::AutoCorrector
 
         MSG = 'Require "# frozen_string_literal: true" in any file that creates a string literal'
+        STRING_TOKEN_TYPES = [:tSTRING, :tSTRING_CONTENT].freeze
 
         def on_new_investigation
           return if processed_source.tokens.empty?
           return unless !frozen_string_literal_comment_exists? && contains_string_literal?
 
-          add_offense(source_range(processed_source.buffer, 0, 0), message: MSG)
+          add_offense(source_range(processed_source.buffer, 0, 0), message: MSG) do |corrector|
+            insert_comment(corrector)
+          end
         end
 
         private
 
         def contains_string_literal?
-          processed_source.tokens.any? { |token| token.type == :tSTRING }
+          processed_source.tokens.any? { |token| STRING_TOKEN_TYPES.any?(token.type) }
+        end
+
+        def insert_comment(corrector)
+          comment = last_special_comment(processed_source)
+
+          if comment
+            corrector.insert_after(processed_source.buffer.line_range(comment.line), "\n#{FROZEN_STRING_LITERAL_ENABLED}")
+          else
+            corrector.insert_before(processed_source.buffer.source_range, "#{FROZEN_STRING_LITERAL_ENABLED}\n")
+          end
+        end
+
+        def last_special_comment(processed_source)
+          token_number = 0
+          token = nil
+          next_token = processed_source.tokens[token_number]
+
+          while next_token.text.start_with?(Style::Encoding::SHEBANG) || MagicComment.parse(next_token.text).any?
+            token_number += 1
+            token = next_token
+            next_token = processed_source.tokens[token_number]
+          end
+
+          token
         end
       end
     end
--- spec/rubocop/cop/performance/require_frozen_string_literal_spec.rb
+++ spec/rubocop/cop/performance/require_frozen_string_literal_spec.rb
@@ -108,12 +108,116 @@ RSpec.describe RuboCop::Cop::Performance::RequireFrozenStringLiteral, :config do
   end
 
   it 'does not have the comment but has an interpolated string' do
-    expect_offense(<<~RUBY)
+    expect_offense(<<~'RUBY')
       # typed: strict
       ^ Require "# frozen_string_literal: true" in any file that creates a string literal
       module Thing
         INTEGER_CONSTANT = 123
         STRING_CONSTANT4 = "hello #{INTEGER_CONSTANT}"
+      end
+    RUBY
+  end
+
+  it 'autocorrects by adding a frozen string literal comment under an encoding comment' do
+    expect_offense(<<~RUBY)
+      # encoding: utf-8
+      ^ Require "# frozen_string_literal: true" in any file that creates a string literal
+      VAL = "hello"
+    RUBY
+
+    expect_correction(<<~RUBY)
+      # encoding: utf-8
+      # frozen_string_literal: true
+      VAL = "hello"
+    RUBY
+  end
+
+  it 'autocorrects by adding a frozen string literal comment under a Sorbet comment' do
+    expect_offense(<<~RUBY)
+      # typed: true
+      ^ Require "# frozen_string_literal: true" in any file that creates a string literal
+
+      VAL = "hello"
+    RUBY
+
+    expect_correction(<<~RUBY)
+      # typed: true
+      # frozen_string_literal: true
+
+      VAL = "hello"
+    RUBY
+  end
+
+  it 'autocorrects by adding a frozen string literal comment under a shebang comment' do
+    expect_offense(<<~RUBY)
+      #!/usr/bin/env ruby
+      ^ Require "# frozen_string_literal: true" in any file that creates a string literal
+
+      VAL = "hello"
+    RUBY
+
+    expect_correction(<<~RUBY)
+      #!/usr/bin/env ruby
+      # frozen_string_literal: true
+
+      VAL = "hello"
+    RUBY
+  end
+
+  it 'autocorrects by adding a frozen string literal comment before non-magic leading comment lines' do
+    expect_offense(<<~RUBY)
+      #!/usr/bin/env ruby
+      ^ Require "# frozen_string_literal: true" in any file that creates a string literal
+      # typed: strict
+      # encoding: utf-8
+      # hey how's it going? 
+
+      VAL = "hello"
+    RUBY
+
+    expect_correction(<<~RUBY)
+      #!/usr/bin/env ruby
+      # typed: strict
+      # encoding: utf-8
+      # frozen_string_literal: true
+      # hey how's it going? 
+
+      VAL = "hello"
+    RUBY
+  end
+
+  it 'autocorrects by adding a frozen string literal comment if there are no leading magic comment lines' do
+    expect_offense(<<~RUBY)
+      module Module1
+      ^ Require "# frozen_string_literal: true" in any file that creates a string literal
+        VAL = "hello"
+      end
+    RUBY
+
+    expect_correction(<<~RUBY)
+      # frozen_string_literal: true
+      module Module1
+        VAL = "hello"
+      end
+    RUBY
+  end
+
+  it 'autocorrects by adding a frozen string literal comment if there are no leading magic comment lines but there are leading empty lines' do
+    expect_offense(<<~RUBY)
+
+      ^{} Require "# frozen_string_literal: true" in any file that creates a string literal
+
+      module Module1
+        VAL = "hello"
+      end
+    RUBY
+
+    expect_correction(<<~RUBY)
+      # frozen_string_literal: true
+
+
+      module Module1
+        VAL = "hello"
       end
     RUBY
   end
